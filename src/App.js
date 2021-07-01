f7887c298071bf8c42e5f41df5c339cf2cacc170
a20l <aidaytyn@gmail.com>
2021-07-01 01:58:23 -0500

    added some components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Detail from './components/Detail';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Router path='/detail'>
+            <Detail />
+          </Router>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      
+      
     </div>
   );
 }
